040a9f164803909f0a29ddb4f0c90e8f36f4f3fd
omaimah-sulaiman <omaymahshakhatrah@gmail.com>
2021-05-23 15:14:28 +0300

    add index,app,style and modifed these pages

---
+++ app.js
@@ -0,0 +1,15 @@
+var q0 = prompt("Please enter your name")
+
+ alert( 'Hello ' +  q0)
+
+let q1=prompt('Do you like agame ?')
+
+if(q1='yes'){
+    let q2= prompt('Do you think you are too noticeable?')
+    if(q2='yes'){
+        let q3= prompt('How much you give yourself from 1 to 10?')
+
+    }
+
+}else q4= prompt('What yor  talent')
+
